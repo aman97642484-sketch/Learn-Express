5c347fd38b12c3f6c034718ff4c5628752f4cb56
Aman Dubey <aman97642484@gmail.com>
2026-04-25 11:20:12 +0530

    first commit

--- project-1/index.js
+++ project-1/index.js
@@ -1,16 +1,38 @@
 const express = require("express");
 const app = express();
 const path = require("path");
+const fs = require("fs");
 
-app.set("view engine", "ejs");
 app.use(express.json());
-app.use(express.static(path.join(__dirname, "public")));
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
+app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
-  res.render("index");
+  fs.readdir(`./files`, (err, files) => {
+    res.render("index", { files: files });
+  });
 });
 
-app.listen(3000, () => {
-  console.log("Example app listening on port 3000!");
+app.get("/files/:filename", (req, res) => {
+  const filename = req.params.filename;
+  fs.readFile(`./files/${filename}`, "utf8", (err, data) => {
+    if (err) {
+      res.status(404).send("File not found");
+    } else {
+      res.render("show", { title: filename, description: data });
+    }
+  });
 });
+
+app.post("/create", (req, res) => {
+  fs.writeFile(
+    `./files/${req.body.title.split(" ").join("-")}.txt`,
+    req.body.description,
+    (err) => {
+      res.redirect("/");
+    },
+  );
+});
+
+app.listen(3000);
